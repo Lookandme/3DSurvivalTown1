eac712ad3d516580a3c7a3cdbbf3b7c4e8c249c8
Lookandme <kgg9386@gmail.com>
2024-10-25 19:19:36 +0900

    [Update] Update Player.cs

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,10 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+public PlayerController controller;
+    private void Awake()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        controller = GetComponent<PlayerController>();
+        CharacterManager.Instance.Player = this;
     }
 }
