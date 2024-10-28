97a796bea8eb675e051ae9da86ad8b0c9b2cf43d
Lookandme <kgg9386@gmail.com>
2024-10-28 10:36:22 +0900

    [Update] Update StaminaUi.cs

--- Assets/Scripts/Ui/PlayerCondition.cs
+++ Assets/Scripts/Ui/PlayerCondition.cs
@@ -1,18 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerCondition : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Image _image;
+
+    [Header("UserCondition")]
+
+    protected float amount = 0f; // 변하는 수치
+    protected float currentValue = 100f; // 현재 수치
+    protected float maxValue = 100f;  // 최대 수치
+    protected float minValue = 0f;  // 최소 수치
+
+    protected virtual void Awake()
     {
-        
+       
     }
 
-    // Update is called once per frame
-    void Update()
+    protected virtual void AddAmount(float amount)
     {
+        currentValue += amount;
+
+        if (currentValue < maxValue)
+        {
+            currentValue = maxValue;
+        }
         
+    }
+    protected virtual void DecreaseAmount(float amount)
+    {
+            currentValue -= amount;
+        if (currentValue < minValue)
+        {
+            currentValue = minValue;
+        }
+       
     }
 }
