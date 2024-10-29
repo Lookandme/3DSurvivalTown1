1afb0432a379a551b141fd292343fb4260b1a733
Lookandme <kgg9386@gmail.com>
2024-10-29 16:11:37 +0900

    [Update] JumpPad

--- Assets/Scripts/JumpPad.cs
+++ Assets/Scripts/JumpPad.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class JumpCube : MonoBehaviour
+public class JumpPad : MonoBehaviour
 {
     public float jumpForce = 100f;
 
